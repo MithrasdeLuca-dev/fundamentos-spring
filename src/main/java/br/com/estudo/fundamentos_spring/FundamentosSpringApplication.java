c7f7c51db9313c3a1c8e28a6c013b573677a29e5
MithrasdeLuca-dev <mithrasdeluca.dev@gmail.com>
2025-03-11 23:31:58 -0300

    Fundamentos spring intellij

---
+++ src/main/java/br/com/estudo/fundamentos_spring/FundamentosSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo.fundamentos_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSpringApplication.class, args);
+	}
+
+}
